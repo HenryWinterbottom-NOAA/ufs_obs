7446b5c7ec5aa9176eed2cf9192d897395e61a94
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2023-10-28 18:44:41 -0600

    Added writing method.

--- sorc/atcf.py
+++ sorc/atcf.py
@@ -38,12 +38,20 @@ History
 
 # ----
 
-# pylint: disable=fixme
+# pylint: disable=arguments-differ
 
 # ----
 
+import os
 from types import SimpleNamespace
+from typing import Dict
 
+from confs.jinja2_interface import write_from_template
+from confs.yaml_interface import YAML
+from tools import fileio_interface, parser_interface
+from utils.decorator_interface import privatemethod
+
+from build_obs import build_obs
 from exceptions import ATCFError
 from observation import Observation
 from obsio.atcf_read import read_tcvfile
@@ -78,6 +86,78 @@ class ATCF(Observation):
 
         # Define the base-class attributes.
         super().__init__(obs_type="atcf")
+        parser_interface.enviro_set(
+            envvar="ATCF_READ_SCHEMA", value=self.obs_type_obj.schema
+        )
+
+    @privatemethod
+    @build_obs
+    def build_tcv(self: Observation, tcvobj: SimpleNamespace) -> Dict:
+        """
+        Description
+        -----------
+
+        This method builds the ATCF-formatted record for the
+        respective ATCF event.
+
+        Parameters
+        ----------
+
+        tcvobj: SimpleNamespace
+
+            A Python SimpleNamespace object containing the attributes
+            for the respective ATCF record.
+
+        Return
+        ------
+
+        obs_dict: Dict
+
+            A Python dictionary containing the ATCF observations.
+
+        """
+
+        # Build the TC-vitals record.
+        schema_path = self.obs_type_obj.schema
+        obs_dict = parser_interface.object_todict(object_in=tcvobj)
+
+        return (schema_path, obs_dict)
+
+    @privatemethod
+    def format_tcv(self: Observation, obs_dict: Dict) -> Dict:
+        """
+        Description
+        -----------
+
+        This method formats the ATCF record in accordance with the
+        ATCF record schema.
+
+        Parameters
+        ----------
+
+        obs_dict: Dict
+
+            A Python dictionary containing the ATCF observations.
+
+        Returns
+        -------
+
+        tcv_dict: Dict
+
+            A Python dictionary containing the ATCF-formatted record.
+
+        """
+
+        # Format the ATCF observations.
+        schema_dict = YAML().read_yaml(yaml_file=self.obs_type_obj.schema)
+        tcv_dict = {}
+        for obs in obs_dict:
+            strfrmt = parser_interface.dict_key_value(
+                dict_in=schema_dict[obs], key="format", force=True, no_split=True
+            )
+            tcv_dict[obs] = strfrmt % obs_dict[obs]
+
+        return tcv_dict
 
     def read(self: Observation, atcf_filepath: str = None) -> SimpleNamespace:
         """
@@ -104,6 +184,14 @@ class ATCF(Observation):
             A Python SimpleNamespace object containing the attributes
             for each ATCF record.
 
+        Raises
+        ------
+
+        ATCFError:
+
+            - raised if the `atcf_filepath` attribute is NoneType upon
+              entry.
+
         """
 
         # Read the ATCF-formatted records.
@@ -115,12 +203,67 @@ class ATCF(Observation):
 
         return tcvobj
 
-    def write(self: Observation, tcvobj: SimpleNamespace) -> None:
+    def write(self: Observation, tcvobj: SimpleNamespace, atcf_filepath: str) -> None:
         """
-        # TODO
+        Description
+        -----------
 
+        This method writes a ATCF-formatted file for the specified
+        ATCF record events.
+
+        Parameters
+        ----------
+
+        tcvobj: SimpleNamespace
+
+            A Python SimpleNamespace object containing the attributes
+            for each ATCF record.
+
+        atcf_filepath: str
+
+            A Python string specifying the path to the ATCF-formatted
+            file to contain the respective ATCF records.
+
+        Raises
+        ------
+
+        ATCFError:
+
+            - raised if an exception is encountered while building,
+              formatting, and/or writing the respective ATCF records.
 
         """
 
-        # TODO
+        # Loop through all ATCF records, format the records, and write
+        # the respective records to the specified file path.
         super().write()
+        msg = f"Writing ATCF records to {atcf_filepath}."
+        self.logger.info(msg=msg)
+        try:
+            with open(atcf_filepath, "w", encoding="utf-8") as atcf_file:
+                for tcid in vars(tcvobj):
+                    msg = f"Building ATCF record for TC {tcid}."
+                    self.logger.info(msg=msg)
+                    obs_dict = self.build_tcv(
+                        tcvobj=parser_interface.object_getattr(
+                            object_in=tcvobj, key=tcid, force=True
+                        )
+                    )
+                    tcv_dict = self.format_tcv(obs_dict=obs_dict)
+                    vfile_jinja = fileio_interface.virtual_file().name
+                    write_from_template(
+                        tmpl_path=self.obs_type_obj.tmpl,
+                        output_file=vfile_jinja,
+                        in_dict=tcv_dict,
+                        rpl_tmpl_mrks=True,
+                        skip_missing=True,
+                    )
+                    with open(vfile_jinja, "r", encoding="utf-8") as vfile:
+                        tcv = vfile.read()
+                    msg = f"Writing ATCF record {tcv}."
+                    self.logger.info(msg=msg)
+                    atcf_file.write(f"{tcv}")
+                    os.unlink(vfile_jinja)
+        except Exception as errmsg:
+            msg = f"Writing the ATCF records failed with error {errmsg}. Aborting!!!"
+            raise ATCFError(msg=msg) from errmsg
