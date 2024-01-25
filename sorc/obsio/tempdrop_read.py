767db47b97646e48ea7cf04de9ffd78f95c3c2a6
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2024-01-25 09:49:07 -0700

    Linter updates.

--- sorc/obsio/tempdrop_read.py
+++ sorc/obsio/tempdrop_read.py
@@ -1,43 +1,85 @@
 """
+Module
+------
+
+    tempdrop_read.py
+
+Description
+-----------
+
+    This module contains function to read TEMPDROP observation
+    messages collected from sondes, particularly those dropped within
+    tropical cyclone events.
+
+Classes
+-------
+
+    TEMPDROP(correct_drift=False, correct_time=False)
+
+        This is the base-class object for decoding TEMPDROP
+        observation messages and providing user specified corrections;
+        it is a sub-class of Observation.
+
+Requirements
+------------
+
+- ufs_pytils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
+
+Author(s)
+---------
+
+    Henry R. Winterbottom; 25 January 2024
+
+History
+-------
+
+    2024-01-25: Henry Winterbottom -- Initial implementation.
+
+References
+----------
+
+    NHOP sonde drop format
+
+    https://www.aoml.noaa.gov/hrd/format/tempdrop_format.html
+
+    Franklin, J. L., S. E. Feuer, J. Kaplan, and S. D. Aberson, 1996:
+    Tropical cyclone motion and surrounding flow relationships:
+    Searching for beta gyres in omega dropwindsonde datasets. Monthly
+    Weather Review, 124, 64-84.
+
+    https://doi.org/10.1175/1520-0493(1996)124<0064:TCMASF>2.0.CO;2
 
 """
 
 # ----
 
-from types import SimpleNamespace
-from typing import Dict, List, Tuple, Generic, Any, Union
-import re
-import os
-import string
-import time
-from geopy.distance import distance, geodesic
-from geopy.point import Point
+# pylint: disable=abstract-method
+# pylint: disable=too-many-instance-attributes
 
-from tools import datetime_interface, parser_interface, fileio_interface
+# ----
 
-from pydoc import locate
-import numpy
-from metpy.units import units
-from utils.timestamp_interface import GLOBAL, GENERAL
-from build_obs import build_obs
-from diags.grids.bearing_geoloc import bearing_geoloc
-from diags.grids.haversine import haversine
-
-
-from utils.decorator_interface import privatemethod
-from observation import Observation
-from utils.schema_interface import build_schema
-from utils.table_interface import init_table, compose
-from confs.yaml_interface import YAML
 import collections
 from operator import itemgetter
+from types import SimpleNamespace
 
-from obsio.sonde.advect import interp_hsa, fallrate, update_time, advect
-from obsio.sonde.hsa import layer_mean, write_hsa, sfcpres, decode, choparr, locations, obslocation, tempdrop, dateinfo
+import numpy
+from confs.yaml_interface import YAML
+from observation import Observation
+from tools import parser_interface
+from utils.decorator_interface import privatemethod
+from utils.schema_interface import build_schema
 
-import statistics
-import math
-
+from obsio.sonde.advect import advect, fallrate, interp_hsa, update_time
+from obsio.sonde.hsa import (
+    choparr,
+    dateinfo,
+    decode,
+    layer_mean,
+    locations,
+    sfcpres,
+    tempdrop,
+    write_hsa,
+)
 
 # ----
 
@@ -46,11 +88,35 @@ __all__ = ["TEMPDROP"]
 
 # ----
 
-class TEMPDROP(Observation):
-    """ """
 
-    def __init__(self: Observation, correct_drift: bool = False,
-                 correct_time: bool = False):
+class TEMPDROP(Observation):
+    """
+    Description
+    -----------
+
+    This is the base-class object for decoding TEMPDROP observation
+    messages and providing user specified corrections; it is a
+    sub-class of Observation.
+
+    Keywords
+    --------
+
+    correct_drift: ``bool``, optional
+
+        Correct the sonde observation locations relative to drift
+        during descent; the observation time will also be corrected
+        relative to the theoretical fallrate for the sonde.
+
+    correct_time: ``bool``, optional
+
+        Correct the sonde observation time relative to the theoretical
+        fallrate for the sonde.
+
+    """
+
+    def __init__(
+        self: Observation, correct_drift: bool = False, correct_time: bool = False
+    ):
         """
         Description
         -----------
@@ -68,10 +134,12 @@ class TEMPDROP(Observation):
         self.psfc_flag = 1070.0
         self.validlevs_list = ["manl", "sigl"]
         self.varname_list = ["hgt", "pres", "rh", "temp", "uwnd", "vwnd"]
-        self.missing_data = -99.
-        self.cls_schema = build_schema(schema_def_dict=YAML().read_yaml(yaml_file=schema_path))
+        self.missing_data = -99.0
+        self.cls_schema = build_schema(
+            schema_def_dict=YAML().read_yaml(yaml_file=schema_path)
+        )
         self.hsa_timestamp_frmt = "%y%m%d. %H%M"
-        
+
     @privatemethod
     def drift(self: Observation, tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
         """
@@ -104,17 +172,21 @@ class TEMPDROP(Observation):
         msg = "Correcting the sonde locations due to drift."
         self.logger.info(msg=msg)
         tempdrop_obj = self.layers(tempdrop_obj=tempdrop_obj)
-        tempdrop_obj.interp.heading = \
-            90.0 + (numpy.degrees(numpy.arctan2(tempdrop_obj.interp.uwnd[:],
-                                         tempdrop_obj.interp.vwnd[:])))
-        tempdrop_obj.interp.dist = (numpy.sqrt(tempdrop_obj.interp.uwnd[:]**2.0 +
-                                               tempdrop_obj.interp.vwnd[:]**2.0))* \
-                                               tempdrop_obj.interp.fallrate[:]
-        tempdrop_obj = advect(tempdrop_obj = tempdrop_obj)
-        tempdrop_obj = update_time(tempdrop_obj = tempdrop_obj)
+        tempdrop_obj.interp.heading = 90.0 + (
+            numpy.degrees(
+                numpy.arctan2(tempdrop_obj.interp.uwnd[:], tempdrop_obj.interp.vwnd[:])
+            )
+        )
+        tempdrop_obj.interp.dist = (
+            numpy.sqrt(
+                tempdrop_obj.interp.uwnd[:] ** 2.0 + tempdrop_obj.interp.vwnd[:] ** 2.0
+            )
+        ) * tempdrop_obj.interp.fallrate[:]
+        tempdrop_obj = advect(tempdrop_obj=tempdrop_obj)
+        tempdrop_obj = update_time(tempdrop_obj=tempdrop_obj)
 
         return tempdrop_obj
-        
+
     @privatemethod
     def layers(self: Observation, tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
         """
@@ -146,14 +218,25 @@ class TEMPDROP(Observation):
         # Compute the TEMPDROP message layer means and sonde fallrate.
         tempdrop_obj.layer = parser_interface.object_define()
         psfc = tempdrop_obj.interp.psfc
-        tempdrop_obj.layer.avgp = choparr(vararr=layer_mean(vararr=tempdrop_obj.interp.pres))
-        tempdrop_obj.layer.avgt = choparr(vararr=layer_mean(vararr=tempdrop_obj.interp.temp))
-        tempdrop_obj.layer.avgu = choparr(vararr=layer_mean(vararr=tempdrop_obj.interp.uwnd))
-        tempdrop_obj.layer.avgv = choparr(vararr=layer_mean(vararr=tempdrop_obj.interp.vwnd))
-        tempdrop_obj.interp.fallrate = interp_hsa(varin=fallrate(avgp=tempdrop_obj.layer.avgp,
-                                                                 avgt=tempdrop_obj.layer.avgt,psfc=psfc),
-                                                  zarr=tempdrop_obj.interp.pres)
-        
+        tempdrop_obj.layer.avgp = choparr(
+            vararr=layer_mean(vararr=tempdrop_obj.interp.pres)
+        )
+        tempdrop_obj.layer.avgt = choparr(
+            vararr=layer_mean(vararr=tempdrop_obj.interp.temp)
+        )
+        tempdrop_obj.layer.avgu = choparr(
+            vararr=layer_mean(vararr=tempdrop_obj.interp.uwnd)
+        )
+        tempdrop_obj.layer.avgv = choparr(
+            vararr=layer_mean(vararr=tempdrop_obj.interp.vwnd)
+        )
+        tempdrop_obj.interp.fallrate = interp_hsa(
+            varin=fallrate(
+                avgp=tempdrop_obj.layer.avgp, avgt=tempdrop_obj.layer.avgt, psfc=psfc
+            ),
+            zarr=tempdrop_obj.interp.pres,
+        )
+
         return tempdrop_obj
 
     @privatemethod
@@ -188,20 +271,26 @@ class TEMPDROP(Observation):
         # Format the TEMPDROP message attributes for the HSA related
         # applications.
         frmtdict = {}
-        for (idx, obs) in enumerate(tempdrop_obj.decode):
+        for idx, obs in enumerate(tempdrop_obj.decode):
             obslist = list(obs.split())
-            obsdict = collections.OrderedDict({key: None for (key, _) in self.cls_schema.items()})
-            for (jdx, item) in enumerate(obsdict):
+            obsdict = collections.OrderedDict(
+                {key: None for (key, _) in self.cls_schema.items()}
+            )
+            for jdx, item in enumerate(obsdict):
                 obsdict[item] = obslist[jdx]
             obsdict = parser_interface.dict_formatter(in_dict=obsdict)
-            frmtdict[idx] = {key: numpy.nan if value == self.missing_data else \
-                             value for (key, value) in obsdict.items()}
+            frmtdict[idx] = {
+                key: numpy.nan if value == self.missing_data else value
+                for (key, value) in obsdict.items()
+            }
         tempdrop_obj.frmtsonde = frmtdict
 
         return tempdrop_obj
 
     @privatemethod
-    def interpsonde(self: Observation, tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
+    def interpsonde(
+        self: Observation, tempdrop_obj: SimpleNamespace
+    ) -> SimpleNamespace:
         """
         Description
         -----------
@@ -237,28 +326,42 @@ class TEMPDROP(Observation):
         varname_list.append("flag")
         for varname in varname_list:
             interp_obj = parser_interface.object_setattr(
-                object_in=interp_obj, key=varname,
-                value=[tempdrop_obj.frmtsonde[idx][varname] for
-                       idx in range(len(tempdrop_obj.frmtsonde))]
-                )
-        idx_list = [idx for idx in range(len(interp_obj.pres)) if \
-                    (interp_obj.pres[idx]) < self.psfc_flag and (interp_obj.flag[idx].lower()
-                                                         in self.validlevs_list)]
+                object_in=interp_obj,
+                key=varname,
+                value=[
+                    tempdrop_obj.frmtsonde[idx][varname]
+                    for idx in range(len(tempdrop_obj.frmtsonde))
+                ],
+            )
+        idx_list = [
+            idx
+            for idx in range(len(interp_obj.pres))
+            if (interp_obj.pres[idx]) < self.psfc_flag
+            and (interp_obj.flag[idx].lower() in self.validlevs_list)
+        ]
         pres = list(itemgetter(*idx_list)(interp_obj.pres))
         tempdrop_obj.interp.flag = list(itemgetter(*idx_list)(interp_obj.flag))
-        tempdrop_obj.interp.hgt = interp_hsa(varin=list(itemgetter(*idx_list)(interp_obj.hgt)),
-                                             zarr=pres)
-        tempdrop_obj.interp.rh = interp_hsa(varin=list(itemgetter(*idx_list)(interp_obj.rh)),
-                                            zarr=pres)
-        tempdrop_obj.interp.temp = interp_hsa(varin=list(itemgetter(*idx_list)(interp_obj.temp)),
-                                              zarr=pres)
-        tempdrop_obj.interp.uwnd = interp_hsa(varin=list(itemgetter(*idx_list)(interp_obj.uwnd)),
-                                              zarr=pres)
-        tempdrop_obj.interp.vwnd = interp_hsa(varin=list(itemgetter(*idx_list)(interp_obj.vwnd)),
-                                              zarr=pres)
-        tempdrop_obj.interp.pres = interp_hsa(varin=list(itemgetter(*idx_list)(interp_obj.pres)),
-                                              zarr=pres)
-        tempdrop_obj.interp.psfc = numpy.array(sfcpres(interp_obj=interp_obj, psfc_flag = self.psfc_flag))
+        tempdrop_obj.interp.hgt = interp_hsa(
+            varin=list(itemgetter(*idx_list)(interp_obj.hgt)), zarr=pres
+        )
+        tempdrop_obj.interp.rh = interp_hsa(
+            varin=list(itemgetter(*idx_list)(interp_obj.rh)), zarr=pres
+        )
+        tempdrop_obj.interp.temp = interp_hsa(
+            varin=list(itemgetter(*idx_list)(interp_obj.temp)), zarr=pres
+        )
+        tempdrop_obj.interp.uwnd = interp_hsa(
+            varin=list(itemgetter(*idx_list)(interp_obj.uwnd)), zarr=pres
+        )
+        tempdrop_obj.interp.vwnd = interp_hsa(
+            varin=list(itemgetter(*idx_list)(interp_obj.vwnd)), zarr=pres
+        )
+        tempdrop_obj.interp.pres = interp_hsa(
+            varin=list(itemgetter(*idx_list)(interp_obj.pres)), zarr=pres
+        )
+        tempdrop_obj.interp.psfc = numpy.array(
+            sfcpres(interp_obj=interp_obj, psfc_flag=self.psfc_flag)
+        )
 
         return tempdrop_obj
 
@@ -284,6 +387,7 @@ class TEMPDROP(Observation):
 
         """
 
+        # Decode and compute the sonde observation attributes.
         tempdrop_obj = parser_interface.object_define()
         tempdrop_obj.filepath = filepath
         tempdrop_obj = tempdrop(tempdrop_obj=tempdrop_obj)
@@ -297,6 +401,3 @@ class TEMPDROP(Observation):
         if self.correct_time:
             tempdrop_obj = update_time(tempdrop_obj=tempdrop_obj)
         write_hsa(tempdrop_obj=tempdrop_obj)
-
-    
-    
