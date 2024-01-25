3ba3bc8c25dc869877b5c96f9ca42f4149d4c77c
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2024-01-25 10:08:47 -0700

    Linter updates.

--- sorc/obsio/sonde/hsa.py
+++ sorc/obsio/sonde/hsa.py
@@ -1,10 +1,14 @@
-"""Module
+"""
+Module
 ------
 
     hsa.py
 
 Description
 -----------
+
+    This module contains functions to manipulate Hurricane Research
+    Division (HRD) Spline Analysis (HSA) formatted observation files.
 
 Functions
 ---------
@@ -72,32 +76,59 @@ History
 
     2024-01-24: Henry Winterbottom -- Initial implementation.
 
+References
+----------
+
+    Franklin, J. L., S. E. Feuer, J. Kaplan, and S. D. Aberson, 1996:
+    Tropical cyclone motion and surrounding flow relationships:
+    Searching for beta gyres in omega dropwindsonde datasets. Monthly
+    Weather Review, 124, 64-84.
+
+    https://doi.org/10.1175/1520-0493(1996)124<0064:TCMASF>2.0.CO;2
+
 """
 
 # ----
 
-import os
-import numpy
-from types import SimpleNamespace
+# pylint: disable=expression-not-assigned
+# pylint: disable=line-too-long
+# pylint: disable=no-name-in-module
+# pylint: disable=undefined-loop-variable
 
-from typing import Tuple
+# ----
+
+import os
 import re
-from tools import parser_interface, fileio_interface, datetime_interface
+import statistics
+from types import SimpleNamespace
+from typing import Tuple
+
+import numpy
+from obsio.sonde.sondelib import close_hsa, drop
+from tools import datetime_interface, fileio_interface, parser_interface
 from utils.logger_interface import Logger
 from utils.timestamp_interface import GLOBAL
-from obsio.sonde.sondelib import drop, close_hsa
-import statistics
 
 # ----
 
 # Define all available module properties.
-__all__ = ["dateinfo", "decode", "layer_mean", "locations", "obslocation", "sfcpres", "tempdrop", "write_hsa"]
+__all__ = [
+    "dateinfo",
+    "decode",
+    "layer_mean",
+    "locations",
+    "obslocation",
+    "sfcpres",
+    "tempdrop",
+    "write_hsa",
+]
 
 # ----
 
 logger = Logger(caller_name=__name__)
 
 # ----
+
 
 def choparr(vararr: numpy.array) -> numpy.array:
     """
@@ -130,7 +161,9 @@ def choparr(vararr: numpy.array) -> numpy.array:
 
     return vararr
 
+
 # ----
+
 
 def dateinfo(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     """
@@ -168,13 +201,16 @@ def dateinfo(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     # observation filename.
     datestr = os.path.basename(tempdrop_obj.filepath).split(".")[0]
     tempdrop_obj.dateinfo = datetime_interface.datestrcomps(
-        datestr=datestr, frmttyp=GLOBAL)
+        datestr=datestr, frmttyp=GLOBAL
+    )
     msg = f"Observation date information determined from TEMPDROP filepath {tempdrop_obj.filepath}."
     logger.info(msg=msg)
 
     return tempdrop_obj
 
+
 # ----
+
 
 def decode(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     """
@@ -194,22 +230,22 @@ def decode(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
 
     Returns
     -------
-    
+
     tempdrop_obj: ``SimpleNamespace``
 
         A Python SimpleNamespace containing the HSA-formatted decoded
         TEMPDROP observation(s).
 
     """
-        
+
     # Decode the TEMPDROP formatted observation message.
-    luidx = 99 
+    luidx = 99
     ftninfile = os.path.join(os.getcwd(), "fort.12")
     ftnoutfile = os.path.join(os.getcwd(), f"fort.{luidx}")
     filelist = [ftninfile, ftnoutfile]
     fileio_interface.removefiles(filelist=filelist)
     for filename in filelist:
-        if fileio_interface.fileexist:
+        if fileio_interface.fileexist(path=filename):
             fileio_interface.removefiles(filelist=[filename])
             msg = f"Removing existing file {ftnoutfile}."
             logger.warn(msg=msg)
@@ -218,9 +254,19 @@ def decode(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     iflags = [2]
     for msgstr in tempdrop_obj.tempdrop:
         if "xx" in msgstr.lower():
-            [drop(luidx, 1, iflag, tempdrop_obj.dateinfo.year_short,
-                  tempdrop_obj.dateinfo.month, tempdrop_obj.dateinfo.day, msgstr,
-                  -9999.) for iflag in iflags]
+            [
+                drop(
+                    luidx,
+                    1,
+                    iflag,
+                    tempdrop_obj.dateinfo.year_short,
+                    tempdrop_obj.dateinfo.month,
+                    tempdrop_obj.dateinfo.day,
+                    msgstr,
+                    -9999.0,
+                )
+                for iflag in iflags
+            ]
     close_hsa(luidx)
     close_hsa(12)
     with open(ftnoutfile, "r", encoding="utf-8") as sondefile:
@@ -233,7 +279,9 @@ def decode(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
 
     return tempdrop_obj
 
+
 # ----
+
 
 def layer_mean(vararr: numpy.array) -> numpy.array:
     """
@@ -265,13 +313,15 @@ def layer_mean(vararr: numpy.array) -> numpy.array:
     # Compute the layer mean for the respective variable array.
     lymnarr = numpy.empty(numpy.shape(vararr))
     for idx in range(1, len(vararr)):
-        pidx = (idx - 1)
+        pidx = idx - 1
         lymnarr[pidx] = statistics.mean([vararr[idx], vararr[pidx]])
     lymnarr = numpy.array(lymnarr)
-            
+
     return lymnarr
 
+
 # ----
+
 
 def locations(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     """
@@ -292,7 +342,7 @@ def locations(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
 
     Returns
     -------
-    
+
     tempdrop_obj: ``SimpleNamespace``
 
         A Python SimpleNamespace object containing the release and
@@ -307,15 +357,15 @@ def locations(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     obinfo_list = []
     for infostr in infostrs_list:
         for item in tempdrop_obj.tempdrop:
-            if any(infostr in item.lower() for infostr in infostrs_list):   
+            if any(infostr in item.lower() for infostr in infostrs_list):
                 obinfo_list.append(item.lower())
         obinfo_str = str(set(obinfo_list)).split()
         try:
-            for (idx, element) in enumerate(obinfo_str):
+            for idx, element in enumerate(obinfo_str):
                 if infostr.lower() in element:
                     break
-            infostr_items = obinfo_str[idx+1:idx+3]
-            (locstr, timestr) = (infostr_items[0], infostr_items[1])
+            infostr_items = obinfo_str[idx + 1 : idx + 3]
+            (locstr, _) = (infostr_items[0], infostr_items[1])
             (lat, lon) = obslocation(locstr=locstr)
             tempdrop_obj.locate = parser_interface.object_setattr(
                 object_in=tempdrop_obj.locate, key=infostr, value=(lat, lon)
@@ -323,10 +373,12 @@ def locations(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
         except (ValueError, IndexError):
             msg = f"TEMPDROP message string {infostr.upper()} could not be located."
             logger.warn(msg=msg)
-                
+
     return tempdrop_obj
 
+
 # ----
+
 
 def obslocation(locstr: str) -> Tuple[float, float]:
     """
@@ -372,7 +424,9 @@ def obslocation(locstr: str) -> Tuple[float, float]:
 
     return (lat, lon)
 
+
 # ----
+
 
 def sfcpres(interp_obj: SimpleNamespace, psfc_flag: float) -> float:
     """
@@ -410,15 +464,20 @@ def sfcpres(interp_obj: SimpleNamespace, psfc_flag: float) -> float:
 
     # Determine the surface pressure from the TEMPDROP message.
     try:
-        psfc_idx = [idx for idx in range(len(interp_obj.pres)) if interp_obj.pres[idx]
-                    == psfc_flag][0]
+        psfc_idx = [
+            idx
+            for idx in range(len(interp_obj.pres))
+            if interp_obj.pres[idx] == psfc_flag
+        ][0]
         psfc = interp_obj.hgt[psfc_idx]
     except (ValueError, IndexError):
         psfc = numpy.nanmax(interp_obj.pres)
 
     return psfc
 
+
 # ----
+
 
 def tempdrop(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     """
@@ -453,10 +512,12 @@ def tempdrop(tempdrop_obj: SimpleNamespace) -> SimpleNamespace:
     for line in tempdrop_obj.tempdrop:
         msg = msg + line + "\n"
     logger.info(msg=msg)
-    
+
     return tempdrop_obj
 
+
 # ----
+
 
 def write_hsa(tempdrop_obj: SimpleNamespace) -> None:
     """
@@ -483,7 +544,7 @@ def write_hsa(tempdrop_obj: SimpleNamespace) -> None:
     logger.info(msg=msg)
     format_str = "{:2d} {:7.1f} {:4d} {:7.3f} {:7.3f} {:7.1f} {:7.1f} {:7.1f} {:8.1f} {:6.1f} {:6.1f} {}\n"
     with open(hsa_outfile, "w", encoding="utf-8") as out:
-        for (idx, _) in enumerate(tempdrop_obj.interp.pres):
+        for idx, _ in enumerate(tempdrop_obj.interp.pres):
             outstr = format_str.format(
                 1,
                 float(tempdrop_obj.interp.yymmdd[idx]),
